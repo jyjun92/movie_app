557d856c342eed96aeaf2ae02e08a692a3ff9dc8
Jea Yoen Jun <jea.jun422@gmail.com>
2020-05-03 13:09:17 -0500

    Updates

---
+++ precache-manifest.dd9a8566b228d6e19bbf7dd580c2ec2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c29e656a38c1016e081f3eba788ac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0700f8467592c622082d",
+    "url": "/static/js/2.217e27ce.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.217e27ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172b1a93889a4373a960",
+    "url": "/static/js/main.f2eaac6d.chunk.js"
+  },
+  {
+    "revision": "459a6262b9a6f053411f",
+    "url": "/static/js/runtime-main.2db5a455.js"
+  },
+  {
+    "revision": "25f2b5692e438380c35aa8a60d055a9c",
+    "url": "/static/media/no-poster.25f2b569.png"
+  }
+]);
